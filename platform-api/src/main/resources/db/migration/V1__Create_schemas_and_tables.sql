df4cb17d59c783646884e1a4806255ac089f87eb
Aidar <zaborstik@gmail.com>
2026-03-01 18:42:26 +0200

    Refactor database migration script for improved readability
    - Cleaned up commented-out code in the SQL migration script, enhancing clarity and maintainability.
    - Ensured consistent formatting for primary key definitions across various tables.
    - Removed unnecessary comments while preserving essential information for future reference.

--- platform-api/src/main/resources/db/migration/V1__Create_schemas_and_tables.sql
+++ platform-api/src/main/resources/db/migration/V1__Create_schemas_and_tables.sql
@@ -27,7 +27,7 @@ CREATE SCHEMA IF NOT EXISTS system;
 --
 -- -- ui_binding: Привязка действия к UI (селектор)
 -- CREATE TABLE IF NOT EXISTS system.ui_binding ( -- не финальные, тут хз
---     action VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
+--     action VARCHAR(36) NOT NULL PRIMARY KEY,
 --     selector VARCHAR(510) NOT NULL,
 --     selector_type VARCHAR(36) NOT NULL,
 --     created_time TIMESTAMP NOT NULL,
@@ -40,24 +40,24 @@ CREATE SCHEMA IF NOT EXISTS system;
 --     ui_binding VARCHAR(36) NOT NULL,
 --     meta_key VARCHAR(36) NOT NULL,
 --     meta_value TEXT,
---     PRIMARY KEY (ui_binding, meta_key), -- юник индекс
+--     PRIMARY KEY (ui_binding, meta_key),
 --     FOREIGN KEY (ui_binding) REFERENCES system.ui_binding(action) ON DELETE CASCADE
 -- );
 
 -- workflow_step: Шаг ЖЦ
 CREATE TABLE IF NOT EXISTS system.workflow_step (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
     internalname VARCHAR(255) NOT NULL, -- имя на английском, типа new in_progres
     displayname VARCHAR(255) NOT NULL, -- отображаемое пользователю имя
-    sortorder INT -- номер по счёту -- индекс
+    sortorder INT -- номер по счёту
 );
 CREATE UNIQUE INDEX IF NOT EXISTS idx_workflow_step_id_uniq ON system.workflow_step(id);
 CREATE INDEX IF NOT EXISTS idx_workflow_step_sortorder ON system.workflow_step(sortorder);
 
 -- workflow: ЖЦ
 CREATE TABLE IF NOT EXISTS system.workflow (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
-    displayname VARCHAR(255) NOT NULL, -- отображаемое пользователю имя -- индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    displayname VARCHAR(255) NOT NULL, -- отображаемое пользователю имя
     firststep VARCHAR(36) NOT NULL, -- первый шаг
     FOREIGN KEY (firststep) REFERENCES system.workflow_step(id)
 );
@@ -66,15 +66,15 @@ CREATE INDEX IF NOT EXISTS idx_workflow_displayname ON system.workflow(displayna
 
 -- attachment: Вложение
 CREATE TABLE IF NOT EXISTS zbrtstk.attachment (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
-    displayname VARCHAR(255) -- отображаемое пользователю имя -- индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    displayname VARCHAR(255) -- отображаемое пользователю имя
 );
 CREATE UNIQUE INDEX IF NOT EXISTS idx_attachment_id_uniq ON zbrtstk.attachment(id);
 CREATE INDEX IF NOT EXISTS idx_attachment_displayname ON zbrtstk.attachment(displayname);
 
 -- plan: План выполнения
 CREATE TABLE IF NOT EXISTS zbrtstk.plan (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
     workflow VARCHAR(36) NOT NULL,
     workflow_step_internalname VARCHAR(255) NOT NULL, -- шаг ЖЦ
     stopped_at_plan_step VARCHAR(36) NOT NULL, -- остановился на шаге плана
@@ -88,13 +88,13 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_plan_id_uniq ON zbrtstk.plan(id);
 
 -- plan_step: Шаги плана
 CREATE TABLE IF NOT EXISTS zbrtstk.plan_step (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
-    plan VARCHAR(36) NOT NULL, -- индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    plan VARCHAR(36) NOT NULL,
     workflow VARCHAR(36) NOT NULL,
     workflow_step_internalname VARCHAR(255) NOT NULL, -- шаг ЖЦ
     entitytype VARCHAR(36) NOT NULL,
     entity_id VARCHAR(36), -- объект дочерней системы, в которой происходят действия
-    sortorder INT NOT NULL, -- порядковый номер шага -- индекс
+    sortorder INT NOT NULL, -- порядковый номер шага
     displayname VARCHAR(255) NOT NULL, -- отображаемое пользователю имя
     created_time TIMESTAMP NOT NULL,
     updated_time TIMESTAMP NOT NULL,
@@ -108,10 +108,10 @@ CREATE INDEX IF NOT EXISTS idx_plan_step_sortorder ON zbrtstk.plan_step(sortorde
 
 -- plan_step_action: связка шаг - действия
 CREATE TABLE IF NOT EXISTS zbrtstk.plan_step_action (
-    plan_step VARCHAR(36) NOT NULL, -- индекс
+    plan_step VARCHAR(36) NOT NULL,
     action VARCHAR(36) NOT NULL,
     meta_value TEXT,
-    PRIMARY KEY (plan_step, action), -- юник индекс
+    PRIMARY KEY (plan_step, action),
     FOREIGN KEY (plan_step) REFERENCES zbrtstk.plan_step(id) ON DELETE CASCADE,
     FOREIGN KEY (action) REFERENCES system.action(id) ON DELETE CASCADE
 );
@@ -121,7 +121,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_plan_step_action_plan_step_action_uniq ON 
 
 -- action_type: Типы действия
 CREATE TABLE IF NOT EXISTS system.action_type (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
     internalname VARCHAR(255) NOT NULL, -- имя на английском, типа open clic
     displayname VARCHAR(255) NOT NULL -- отображаемое пользователю имя
 );
@@ -129,12 +129,12 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_action_type_id_uniq ON system.action_type(
 
 -- action: Действия платформы
 CREATE TABLE IF NOT EXISTS system.action (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
     displayname VARCHAR(255) NOT NULL, -- отображаемое пользователю имя
-    internalname VARCHAR(255) NOT NULL, -- ??
+    internalname VARCHAR(255) NOT NULL,
     meta_value TEXT,
     description VARCHAR(255), -- описание для пользователя
-    action_type VARCHAR(36) NOT NULL, -- индекс
+    action_type VARCHAR(36) NOT NULL,
     created_time TIMESTAMP NOT NULL,
     updated_time TIMESTAMP NOT NULL,
     FOREIGN KEY (action_type) REFERENCES system.action_type(id)
@@ -146,7 +146,7 @@ CREATE INDEX IF NOT EXISTS idx_action_action_type ON system.action(action_type);
 CREATE TABLE IF NOT EXISTS system.action_applicable_entity_type (
     action VARCHAR(36) NOT NULL,
     entity_type VARCHAR(36) NOT NULL, -- тип объекта
-    PRIMARY KEY (action, entity_type), -- юник индекс
+    PRIMARY KEY (action, entity_type),
     FOREIGN KEY (action) REFERENCES system.action(id) ON DELETE CASCADE,
     FOREIGN KEY (entity_type) REFERENCES zbrtstk.entity_type(id) ON DELETE CASCADE
 );
@@ -154,8 +154,8 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_action_applicable_entity_type_uniq ON syst
 
 -- execution_result: Итог выполнения плана
 CREATE TABLE IF NOT EXISTS zbrtstk.plan_result (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
-    plan VARCHAR(36) NOT NULL, -- индекс ??
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    plan VARCHAR(36) NOT NULL,
     success BOOLEAN NOT NULL,
     started_time TIMESTAMP NOT NULL,
     finished_time TIMESTAMP NOT NULL,
@@ -167,8 +167,8 @@ CREATE INDEX IF NOT EXISTS idx_plan_result_plan ON zbrtstk.plan_result(plan);
 
 -- execution_log_entry: Лог по шагам
 CREATE TABLE IF NOT EXISTS zbrtstk.plan_step_log_entry ( -- создаётся в случае падения
-    id VARCHAR(36) NOT NULL PRIMARY KEY, -- юник индекс
-    plan VARCHAR(36) NOT NULL, -- индекс
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    plan VARCHAR(36) NOT NULL,
     plan_step VARCHAR(36) NOT NULL,
     plan_result VARCHAR(36) NOT NULL,
     action VARCHAR(36) NOT NULL,
